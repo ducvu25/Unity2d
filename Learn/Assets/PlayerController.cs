49c2b9a38782b35f4b6429a9b37ae8fdf5182430
ducvu25 <vuduc25022003@gmail.com>
2023-06-29 05:15:54 +0700

    Day1

--- Learn/Assets/PlayerController.cs
+++ Learn/Assets/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10;
+    [SerializeField] float steerSpeed = 30;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.Run();
+    }
+    void Run()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
